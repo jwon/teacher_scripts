fe6bcc0276a59e6a78e4514f406d3aad349e64df
James Won <me@jwon.me>
2021-12-11 13:51:19 -0800

    Add create_groups script

---
+++ create_groups.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import math
+import random
+import os
+import sys
+
+
+scores = []
+with open(sys.argv[1]) as f:
+    for line in f:
+        first, last, score = line.strip().split(',')
+        scores.append((first, last, float(score)))
+scores.sort(key=lambda x: x[2])
+# for student in scores:
+#     print(student)
+# print(len(scores))
+
+NUM_GROUPS = int(input('How many groups do you want? '))
+students_per_group = math.ceil(len(scores) / NUM_GROUPS)
+
+group_by_score = []
+for i in range(students_per_group):
+    group_by_score.append(scores[i*NUM_GROUPS:min((i+1)*NUM_GROUPS, len(scores))])
+
+# for group in group_by_score:
+#     for student in group:
+#         print(student)
+#     print(len(group))
+
+while True:
+    os.system('clear')
+    # Shuffle the order
+    for group in group_by_score:
+        random.shuffle(group)
+    
+    groups = {i: [] for i in range(NUM_GROUPS)}
+    
+    for group in group_by_score:
+        for idx, student in enumerate(group):
+            groups[idx].append(student)
+    
+    for group_num, group in groups.items():
+        print(f'Group #{group_num+1}')
+        for student in group:
+            print(f'{student[0]} {student[1]}: {student[2]}')
+        print()
+
+    if input('Accept groups? [y/n]').strip() == 'y':
+        break
