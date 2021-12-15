c5a70f58d00f39662fb5395fa0b8c6e1e6cbf4b3
James Won <me@jwon.me>
2021-12-15 01:05:26 -0800

    Add table

--- create_groups.py
+++ create_groups.py
@@ -4,6 +4,21 @@ import random
 import os
 import sys
 
+from tabulate import tabulate
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
 
 scores = []
 with open(sys.argv[1]) as f:
@@ -15,11 +30,11 @@ scores.sort(key=lambda x: x[2])
 #     print(student)
 # print(len(scores))
 
-NUM_GROUPS = int(input('How many groups do you want? '))
-students_per_group = math.ceil(len(scores) / NUM_GROUPS)
+ROLES = ['Facilitator', 'Team Captain', 'Recorder/Reporter', 'Resource Manager']
+NUM_GROUPS = math.ceil(len(scores) / len(ROLES))
 
 group_by_score = []
-for i in range(students_per_group):
+for i in range(len(ROLES)):
     group_by_score.append(scores[i*NUM_GROUPS:min((i+1)*NUM_GROUPS, len(scores))])
 
 # for group in group_by_score:
@@ -38,13 +53,16 @@ while True:
     for group in group_by_score:
         for idx, student in enumerate(group):
             groups[idx].append(student)
-    
-    for group_num, group in groups.items():
-        print(f'Group #{group_num+1}')
+    for group in groups.values():
         random.shuffle(group)
-        for (first_name, last_name, score) in group:
-            print(f'{first_name} {last_name}')
-        print()
+    
+    table = [[color.PURPLE + role + color.END] for role in ROLES]
+    headers = [color.UNDERLINE + 'Group Roles' + color.END] + [color.UNDERLINE + f'Group {i+1}' + color.END for i in groups]
+    for group_num, group in groups.items():
+        for idx, (first_name, last_name, score) in enumerate(group):
+            table[idx].append(f'{first_name} {last_name}')
+
+    print(tabulate(table, headers=headers, tablefmt="pretty"))
 
     if input('Accept groups? [y/n]').strip() == 'y':
         break
